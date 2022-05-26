77e930323f35ee3146ca07e0dbb7c2455bcd597f
olivia <oliviabir19@gmail.com>
2022-05-26 14:35:08 -0700

    signUp thunk added

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -40,6 +40,23 @@ export const restoreUser = () => async (dispatch) => {
   return response;
 }
 
+export const signUp = (user) => async (dispatch) => {
+  const { username, email, password} = user
+
+  const response = await csrfFetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      email,
+      password
+    })
+  })
+
+  const data = await response.json()
+  dispatch(setUser(data.user))
+  return response
+}
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
