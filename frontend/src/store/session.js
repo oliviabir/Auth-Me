be0ab76ead7d0eafbc8ec00d7be0ef63852d352d
olivia <oliviabir19@gmail.com>
2022-05-26 14:26:13 -0700

    added restoreUser thunk

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -27,10 +27,18 @@ export const login = (user) => async (dispatch) => {
     }),
   });
 
-  const data = await response.json();
-  dispatch(setUser(data.user));
+  const data = await response.json()
+  dispatch(setUser(data.user))
   return response;
 };
+
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session')
+
+  const data = await response.json()
+  dispatch(setUser(data.user))
+  return response;
+}
 
 const initialState = { user: null };
 
