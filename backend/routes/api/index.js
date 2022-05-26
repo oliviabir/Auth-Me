12eb8d4d5549aeb2418899b97f12a774c42c28f2
olivia <oliviabir19@gmail.com>
2022-05-26 10:43:08 -0700

    added user model and methods

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+
 
 
 module.exports = router;
