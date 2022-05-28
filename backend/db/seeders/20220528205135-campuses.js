a5c52a725924b800bf6408fbaf7c10f77d3dd7d7
olivia <oliviabir19@gmail.com>
2022-05-28 14:05:10 -0700

    began campus seeder files

---
+++ backend/db/seeders/20220528205135-campuses.js
@@ -0,0 +1,268 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert('Campuses', [
+      {
+        id: 1,
+        userId: 1,
+        name: 'Stanford',
+        city: 'Stanford',
+        state: 'California',
+        description: 'Stanford University, officially Leland Stanford Junior University, is a private research university. The campus occupies 8,180 acres, among the largest in the United States, and enrolls over 17,000 students. Located 35 miles south of San Francisco and 20 miles north of San Jose, Stanford University is in the heart of Northern California\’s dynamic Silicon Valley, home to Yahoo, Google, Hewlett-Packard, and many other cutting-edge tech companies that were founded by and continue to be led by Stanford alumni and faculty. Nicknamed the “billionaire factory”, it is said that if Stanford graduates formed their own country it would boast one of the world\’s largest ten economies. Covering 8,180 acres, Stanford has one of the largest university campuses in the US, with 18 interdisciplinary research institutes and seven schools: the Graduate School of Business; School of Earth, Energy & Environmental Sciences; Graduate School of Education; School of Engineering; School of Humanities and Sciences; Law School; and School of Medicine.',
+        tuition: 56169,
+        public: false,
+        private: true
+      },
+      {
+        id: 2,
+        userId: 2,
+        name: 'Columbia University',
+        city: 'New York',
+        state: 'New York',
+        description: 'Established in 1754, Columbia University is a private Ivy League research university in Upper Manhattan, New York City. It was established as King\'s College by royal charter of George II of Great Britain and renamed Columbia College in 1784 following the American Revolutionary War. With an undergraduate acceptance rate of 5.8 percent, Columbia is currently the third most selective college in the United States and the second most selective in the Ivy League after Harvard. Its first president was none other than the literary great Samuel Johnson, and over the years Columbia has produced numerous distinguished alumni, from Oscar winners and Nobel laureates to Supreme Court judges. Three US Presidents and the authors of the Declaration of Independence and American Constitution were also schooled at Columbia. It also runs the highly distinguished Pulitzer Prize, an annual award for achievements in journalism, literature and musical composition. The university is organized into 20 schools, including undergraduate schools such as Columbia College, the Fu Foundation School of Engineering and Applied Science, and the School of General Studies, as well as graduate schools such as Columbia Law School, Columbia College of Physicians and Surgeons, Columbia Journalism School and Columbia Business School. It also had global research outposts across the world. Its total student body numbers around 28,000 and is comprised mainly of postgraduates, with roughly 8,500 undergraduate students. Columbia\’s main campus is Morningside Heights, occupying around six city blocks in the Morningside Heights district of New York. It\’s home to the neo-classical Butler library, one of the largest buildings on campus, and almost two dozen undergraduate dormitories. The university also owns 7,800 apartments in the local area, which house faculty members, students, and staff',
+        tuition: 63530,
+        public: false,
+        private: true
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+      {
+        id: 1,
+        userId: 1,
+        name: '',
+        city: '',
+        state: '',
+        description: '',
+        tuition: 1,
+        inStateTuition: 1,
+        public: true,
+        private: false
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
