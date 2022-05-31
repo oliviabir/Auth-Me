57925172048309e9683ef90146752a00c023f43a
olivia <oliviabir19@gmail.com>
2022-05-31 09:10:02 -0700

    changes in routes

--- backend/routes/api/campus.js
+++ backend/routes/api/campus.js
@@ -4,23 +4,77 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Campus } = require('../../db/models');
+const { Campus, Image } = require('../../db/models');
 
 const router = express.Router();
 
-router.get('/', asyncHandler((req, res) => {
-    const campuses = await Campus.findAll()
+router.get('/', asyncHandler(async(req, res) => {
+    const campuses = await Campus.findAll({
+        include: Image
+    })
 
     return res.json(campuses)
 }))
 
-router.get('/:id(\\d+)', asyncHandler((req, res) => {
+router.get('/:id(\\d+)', asyncHandler(async(req, res) => {
     const campusId = req.params.campusId
 
-    const campus = await Campus.findByPk(campusId)
+    const campus = await Campus.findByPk(campusId, {
+        include: Image
+    })
 
     return res.json(campus)
 }))
 
+const campusValidators = [
+    check('name')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Name')
+        .isLength({ max: 100 })
+        .withMessage('Name must not be longer than 100 characters'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for City')
+        .isLength({ max: 50 })
+        .withMessage('Name must not be longer than 50 characters'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for State')
+        .isLength({ max: 50 })
+        .withMessage('Name must not be longer than 50 characters'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Description'),
+    check('tuiton')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Tuition'),
+    handleValidationErrors
+]
+
+router.post('/new', campusValidators, asyncHandler((req, res) => {
+    const {
+        name,
+        city,
+        state,
+        description,
+        tuition,
+        inStateTuition,
+        public,
+        private
+    } = req.body
+
+    const campus = Campus.build({
+        name,
+        city,
+        state,
+        description,
+        tuition,
+        inStateTuition,
+        public,
+        private
+    })
+
+    return res.json(campus)
+}))
 
 module.exports = router;
