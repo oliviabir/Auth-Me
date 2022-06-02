6fd527388d79af2c03f281c1aa875ec839fb8549
olivia <oliviabir19@gmail.com>
2022-06-01 21:22:12 -0700

    testing campus list/card component

--- frontend/src/components/CampusesList/index.js
+++ frontend/src/components/CampusesList/index.js
@@ -3,16 +3,23 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { getCampusList } from '../../store/campus';
-import AddCampusForm from '../AddCampusForm';
 import CampusCard from './CampusCard';
 import './CampusContainer.css'
 
 const CampusList = () => {
+  //   const dispatch = useDispatch()
+  //   const campuses = useSelector(state => {
+  //       return Object.values(state.campus)
+  //   })
+
+  //   useEffect(() => {
+  //     dispatch(getCampusList())
+  // }, [dispatch])
 
     return (
       <div className='campus-container'>
             <CampusCard />
-            <a href='/campus/new'>
+            <a href='/campus/new' id='add-campus-btn'>
                 Add a Campus
             </a>
       </div>
