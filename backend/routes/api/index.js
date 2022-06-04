79b6899227e15f38c3814703ecc63fdb00107646
olivia <oliviabir19@gmail.com>
2022-06-03 18:07:37 -0700

    work on profile and splash

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,13 +3,11 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const campusRouter = require('./campus.js')
 const bookingRouter = require('./bookings.js')
-const imageRouter = require('./image.js')
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/campus', campusRouter);
 router.use('/bookings', bookingRouter);
-router.use('/image', imageRouter)
 
 
 
--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as sessionActions from '../../store/session';
 
 const Splash = () => {
+  const dispatch = useDispatch()
+
+  const [errors, setErrors] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setErrors([])
+
+    return dispatch(sessionActions.login({ credential: 'DemoUser', password: 'password' }))
+      .catch(async (res) => {
+        const data = await res.json()
+        if (data && data.errors) setErrors(data.errors)
+      })
+  }
+
   return (
-    <>
+    <div className='splash-container'>
+      <h1>Find yourself on campus.</h1>
+      <h2>Discover campuses around the country and book a tour.</h2>
+      <button type='submit' onClick={handleSubmit}>Demo Login</button>
       <h1>Splash</h1>
       <h1>Splash</h1>
-      <h1>Splash</h1>
-      <h1>Splash</h1>
-      <h1>Splash</h1>
-    </>
+    </div>
   )
 }
 
