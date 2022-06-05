69151d4c4b390ee2767d785681d36b5934a00bbd
olivia <oliviabir19@gmail.com>
2022-06-05 15:55:54 -0700

    fixed styling for newly added campus

--- frontend/src/components/CampusImageDetail/index.js
+++ frontend/src/components/CampusImageDetail/index.js
@@ -13,14 +13,14 @@ const CampusImageDetail = ({campus}) => {
               {
                 imagesArr[1] ?
                 <img id='second-image' src={imagesArr[1].url} />
-                : <img src='https://images.unsplash.com/photo-1590012314607-cda9d9b699ae?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MzU3NTE5Nw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080'/>
+                : <img id='second-image' src='https://images.unsplash.com/photo-1590012314607-cda9d9b699ae?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MzU3NTE5Nw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080'/>
               }
           </div>
           <div className='image-three-container'>
               {
                 imagesArr[2] ?
                 <img id='third-image' src={imagesArr[2].url} />
-                : <img src='https://images.unsplash.com/photo-1606761568499-6d2451b23c66?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYyODI4Mjg4Mw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080' />
+                : <img id='third-image' src='https://images.unsplash.com/photo-1606761568499-6d2451b23c66?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYyODI4Mjg4Mw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080' />
               }
           </div>
       </div>
