67246cccd848c71018147e098db9519ff0cae6c0
olivia <oliviabir19@gmail.com>
2022-06-04 19:22:20 -0700

    styling for add campus form

--- frontend/src/components/AddCampusForm/index.js
+++ frontend/src/components/AddCampusForm/index.js
@@ -56,68 +56,82 @@ const AddCampusForm = () => {
 
     return (
         <div>
-            <form onSubmit={handleSubmit} className='form'>
+            <form onSubmit={handleSubmit} className='add-campus-form'>
                 {/* <ul>
                     {errors.map((error, idx) => <li key={idx}>{error}</li>)}
                 </ul> */}
-                <label>School Name
-                    <input
-                        type='text'
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>City
-                    <input
-                        type='text'
-                        value={city}
-                        onChange={(e) => setCity(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>State
-                    <input
-                        type='text'
-                        value={state}
-                        onChange={(e) => setState(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>Description
-                    <input
-                        type='textarea'
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>Tuition
-                    <input
-                        type='number'
-                        value={tuition}
-                        onChange={(e) => setTuition(e.target.value)}
-                        required
-                    />
-                </label>
-                <div>
-                    <input
-                        type='text'
-                        value={url}
-                        required
-                        placeholder='Enter Image URL'
-                        onChange={(e) => setUrl(e.target.value)}
-                    />
-                    <input
-                        type='text'
-                        value={alt}
-                        required
-                        placeholder='Description of Image'
-                        onChange={(e) => setAlt(e.target.value)}
-                    />
+                <h1>Add a Campus</h1>
+                <div className='add-form-container' style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1541339907198-e08756dedf3f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit)'}}>
+                    <label className='add-campus-label' id='add-name-label'>School Name
+                        <input
+                            className='add-campus-input'
+                            type='text'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label className='add-campus-label' id='add-city-label'>City
+                        <input
+                            className='add-campus-input'
+                            type='text'
+                            value={city}
+                            onChange={(e) => setCity(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label className='add-campus-label' id='add-state-label'>State
+                        <input
+                            className='add-campus-input'
+                            type='text'
+                            value={state}
+                            onChange={(e) => setState(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label className='add-campus-label' id='add-description-label'>Description
+                        <input
+                            className='add-campus-input'
+                            type='textarea'
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label className='add-campus-label' id='add-tuition-label'>Tuition
+                        <input
+                            className='add-campus-input'
+                            type='number'
+                            value={tuition}
+                            onChange={(e) => setTuition(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label className='add-campus-label' id='add-image-label'>Campus Image
+                        <input
+                            className='add-campus-input'
+                            type='text'
+                            value={url}
+                            required
+                            placeholder='Enter Image URL'
+                            onChange={(e) => setUrl(e.target.value)}
+                        />
+                    </label>
+                    <label className='add-campus-label' id='add-imgdesc-label'>Image Description
+                        <input
+                            className='add-campus-input'
+                            type='text'
+                            value={alt}
+                            required
+                            placeholder='Description of Image'
+                            onChange={(e) => setAlt(e.target.value)}
+                        />
+                    </label>
                 </div>
-                <Link to='/campus'>Cancel</Link>
-                <button type='submit'>Submit</button>
+                <div className='add-form-buttons'>
+                    <Link to='/campus' className='add-cancel-link'>Cancel</Link>
+                    <button type='submit' className='add-submit-btn'>Submit</button>
+                </div>
             </form>
         </div>
     )
